f78aa42d5fb326902a0d311c818010eb278ce5ed
ern0 <erno.zalka@heitec.hu>
2020-12-02 13:08:54 +0100

    updated s.bat

--- s.bat
+++ s.bat
@@ -1,5 +1,8 @@
-@eco off
+@echo off
 cls
 echo off
-python -B forcom.py
-erase __pycache__ /s /q > nul
+python -B app/forcom.py song/a.txt
+erase __pycache__ /s /q 2> nul
+rmdir __pycache__ /q 2> nul
+erase parser.out 2> nul
+erase parsetab.py 2> nul
