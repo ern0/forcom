01df3e8d41d136c88678bbca22ac4317f92cd843
ern0 <erno.zalka@heitec.hu>
2020-12-03 10:53:57 +0100

    fixed erasing tmp files

--- s.bat
+++ s.bat
@@ -4,5 +4,5 @@ echo off
 python -B app/forcom.py song/a.txt
 erase __pycache__ /s /q 2> nul
 rmdir __pycache__ /q 2> nul
-erase parser.out 2> nul
-erase parsetab.py 2> nul
+erase app\parser.out 2> nul
+erase app\parsetab.py 2> nul
