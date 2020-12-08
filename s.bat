d7bc8b1deb692f681c2602661c5b8f476ee9c140
ern0 <erno.zalka@heitec.hu>
2020-12-08 09:32:09 +0100

    fixed s.bat

--- s.bat
+++ s.bat
@@ -1,8 +1,8 @@
 @echo off
 cls
 echo off
-python -B app/forcom.py song/a.txt
+python -B app/forcom.py song/test.txt graph
 erase __pycache__ /s /q 2> nul
 rmdir __pycache__ /q 2> nul
-erase app\parser.out 2> nul
-erase app\parsetab.py 2> nul
+rem erase app\parser.out 2> nul
+rem erase app\parsetab.py 2> nul
